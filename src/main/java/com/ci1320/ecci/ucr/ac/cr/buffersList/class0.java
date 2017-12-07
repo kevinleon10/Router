d9fcbd3ae238acb489b787d8b98114624e0133e1
Jorge Remón <jorge.remon@ucr.ac.cr>
2017-12-06 18:08:19 -0600

    Reestructured project

---
+++ src/main/java/com/ci1320/ecci/ucr/ac/cr/buffersList/class0.java
@@ -0,0 +1,7 @@
+package com.ci1320.ecci.ucr.ac.cr.buffersList;
+
+/**
+ * Created by JorgeRemon on 6/12/17.
+ */
+public class class0 {
+}
---
+++ src/main/java/com/ci1320/ecci/ucr/ac/cr/router/class0.java
@@ -0,0 +1,7 @@
+package com.ci1320.ecci.ucr.ac.cr.router;
+
+/**
+ * Created by JorgeRemon on 6/12/17.
+ */
+public class class0 {
+}
